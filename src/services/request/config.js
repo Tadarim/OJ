3398ed9f5094273524b126a6ee86d456c9d3b771
gxd3 <guoxiaodong167@gmail.com>
2023-07-20 15:53:45 +0800

    feat: 进行本地数据的模拟

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,2 +1,5 @@
-export const BASE_URL = "http://192.168.50.28:9090"
+export const BASE_URL = process.env.REACT_APP_API_URL
 export const TIMEOUT = 10000
+
+console.log(process.env.REACT_APP_API_URL)
+console.log(process.env.NODE_ENV)
